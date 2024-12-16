f3e3917316e26422a9b69e0dbdef8031ab1e8df6
starx <mail@mrnepal.com>
2024-12-16 15:23:18 +0000

    endpoint for getting channels

--- rxchat/server/api.py
+++ rxchat/server/api.py
@@ -1,5 +1,6 @@
 from fastapi import WebSocket, APIRouter
 from rxchat.server.chat_server import ChatServer
+from typing import List
 import uuid
 from .events import Conversation
 chat_server = ChatServer()
@@ -14,7 +15,10 @@ async def connect_chat(websocket: WebSocket):
     finally:
         await chat_server.handle_user_disconnected(username)
 
-
 @router.get("/chat/{conversation_id}")
 async def get_conversation_id(conversation_id: str) -> Conversation:
     return chat_server.conversations[conversation_id].tail(10)
+
+@router.get("/channels", response_model=List[str])
+async def get_channels():
+    return ["Welcome", "Jokes", "Tech"]
